1057bb4af6166fa936d38c0635ef9b8b8664f19b
Kabir Kapur <kabir@Kabirs-MacBook-Pro.local>
2024-02-17 17:19:39 -0800

    init react project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { ChakraProvider } from '@chakra-ui/react';
+import { header } from './components/header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <header/>
+      <div className="App">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+      </div>
+    </ChakraProvider>
   );
 }
 
---
+++ src/components/header.js
@@ -0,0 +1,7 @@
+import { Divider, Heading } from '@chakra-ui/react';
+
+function Header() {
+    return (
+        <Heading>Hey Fucker</Heading>
+    )
+}
